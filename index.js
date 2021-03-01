41198bb07ac5ecb1174d21071501f931f92de7c7
Andy Weiss <andyweiss1982@gmail.com>
2021-03-01 15:20:14 -0500

    add react to project'

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("HELLO");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Hello = () => <h1>Hello World</h1>;
+
+ReactDOM.render(<Hello />, document.querySelector("#root"));
